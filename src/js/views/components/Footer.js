389984c8af376a85d86f8b86467247249c4933ee
Евгений Лагутин <Runni007@yandex.ru>
2020-06-16 21:02:51 +0400

    feat: add footer and header design

--- src/js/views/components/Footer.js
+++ src/js/views/components/Footer.js
@@ -1,13 +1,23 @@
+import '../../../css/footer.scss';
+
 const Footer = {
   render: async () => {
     const view =  /*html*/`
-        <footer class="footer">
-            <div class="footer__content">
-                <p>
-                    This is footer
-                </p>
-            </div>
-        </footer>
+    <div class="wrapper">
+      <div class="footer__content">
+        <div class="footer__links">
+          <ul class="footer__navList">
+            <li class="footer__navItem"><a href="#/games">Games</a></li>
+            <li class="footer__navItem"><a href="#/dictionary">Dictionary</a></li>
+            <li class="footer__navItem"><a href="#/statistic">Statistic</a></li>
+            <li class="footer__navItem"><a href="#/about">About</a></li>
+          </ul>
+        </div>
+        <div class="footer__copyright">
+          <span>© 2020 RSSchool Students</span>
+        </div>
+      </div>
+    </div>
         `
     return view;
   },
