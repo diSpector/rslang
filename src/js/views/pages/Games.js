6d8daea32cbc9bf34b4feb0538b1db782a05893c
Евгений Лагутин <Runni007@yandex.ru>
2020-06-17 21:25:18 +0400

    feat: add routing for games

--- src/js/views/pages/Games.js
+++ src/js/views/pages/Games.js
@@ -1,12 +1,27 @@
+
+import Utils from '../../services/Utils';
+import Audition from './games/audition';
+import Error404 from './Error404';
+
+const gamesList = {
+  audition: Audition, // Аудиовызов
+};
 const Games = {
   render: async () => {
+    const content = null || document.querySelector('.content');
+    const request = Utils.parseRequestURL();
+    const gameName = request.id;
+    if (gameName) {
+      const page = gamesList[gameName] ? gamesList[gameName] : Error404;
+      content.innerHTML = await page.render();
+      await page.afterRender();
+      return content.innerHTML;
+    }
     const view = /* html */`
-
-          `;
+          <h1>Games</h1>
+    `;
     return view;
   },
   afterRender: async () => { },
-
 };
-
 export default Games;
---
+++ src/js/views/pages/games/audition.js
@@ -0,0 +1,12 @@
+const Audition = {
+
+  render: async () => {
+    const view = /* html */`
+            <h1>Audition Game</h1>
+        `;
+    return view;
+  },
+  afterRender: async () => {
+  },
+};
+export default Audition;
