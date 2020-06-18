6a47394b1a6602abe9819d131ea0fe62ab211751
mironovahl <mironovahl@yandex.ru>
2020-06-19 00:37:32 +0400

    fix: css game

--- src/js/views/pages/games/Sprint/Sprint.js
+++ src/js/views/pages/games/Sprint/Sprint.js
@@ -16,27 +16,29 @@ const Sprint = {
     Sprint.beforeRender();
 
     const view = `
-    <div class="sprint--game">
-      <div class="sprint--game__result">0</div>
-      <div class="sprint--game__card">
-        <div class="sprint--card__title">
-          <span></span>
-          <span></span>
-          <span></span>
+    <div class="sprint">
+      <div class="sprint--game">
+        <div class="sprint--game__result">0</div>
+        <div class="sprint--game__card">
+          <div class="sprint--card__title">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <img class="list" src="./img/list.png" alt="">
+          <img class="list2" src="./img/list2.png" alt="">
+          <div class="sprint--card__word">
+            <p class="word">word</p>
+            <p class="translate">слово</p>
+          </div>
+          <div class="sprint--card__button">
+            <button class="sprint--button__correct">Верно</button>
+            <button class="sprint--button__error">Неверно</button>
+          </div>
         </div>
-        <img class="list" src="./img/list.png" alt="">
-        <img class="list2" src="./img/list2.png" alt="">
-        <div class="sprint--card__word">
-          <p class="word">word</p>
-          <p class="translate">слово</p>
-        </div>
-        <div class="sprint--card__button">
-          <button class="sprint--button__correct">Верно</button>
-          <button class="sprint--button__error">Неверно</button>
-        </div>
+        <div class="sprint--game__arrow"></div>
+        <div class="sprint--game__time">60</div>
       </div>
-      <div class="sprint--game__arrow"></div>
-      <div class="sprint--game__time">60</div>
     </div>
     `;
     return view;
