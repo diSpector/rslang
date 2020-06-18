5e3231ea60e0c02b009744369d6e87017772775a
dispector <dmitry_proekt@mail.ru>
2020-06-18 03:23:50 +0300

    add: game router, markup for enlish puzzle

--- src/js/views/pages/Games.js
+++ src/js/views/pages/Games.js
@@ -1,11 +1,47 @@
-const Games = {
-  render: async () => {
-    const view = /* html */`
+import Utils from '../../services/Utils';
+import EnglishPuzzle from './games/english-puzzle/EnglishPuzzle';
+import Error404 from './Error404';
 
-          `;
+const gameList = {
+  // 'audition'        : Audition,
+  'english-puzzle'  : EnglishPuzzle,
+  // 'speakit'         : SpeakIt,
+  // 'savanna'         : Savanna,
+  // 'sprint'          : Sprint,
+  // 'square'          : Square,
+};
+
+const Games = {
+  currentGame: null,
+
+  beforeRender: function () { // установить игру
+    this.setGame();
+    this.clearHeaderAndFooter();
+  },
+
+  clearHeaderAndFooter: function () {
+    Utils.clearBlock('.header');
+    Utils.clearBlock('.footer');
+  },
+
+  setGame: function () { // распарсить url, установить игру в св-во объекта
+    const request = Utils.parseRequestURL();
+    const { id : gameId } = request;
+    const game = gameList[gameId];
+    const page = (game) ? gameList[gameId] : Error404;
+    this.currentGame = page;
+  },
+
+  render: async () => {
+    Games.beforeRender();
+    
+    const view = /* html */ await Games.currentGame.render();
     return view;
   },
-  afterRender: async () => { },
+
+  afterRender: async () => { 
+    await Games.currentGame.afterRender();
+  },
 
 };
 
