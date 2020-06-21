e386402988f16ac75576c50102b60f97ba915a19
Kate <chelsea-92@mail.ru>
2020-06-21 18:19:47 +0300

    feat: add styles and layout

--- src/js/views/pages/games/savannah/Savannah.js
+++ src/js/views/pages/games/savannah/Savannah.js
@@ -1,5 +1,6 @@
 import '../../../../../css/pages/games/savannah/savannah.scss';
 import '../../../../../css/pages/games/allGames.scss';
+
 import Utils from '../../../../services/Utils';
 import Game from '../Game';
 
@@ -19,20 +20,38 @@ const Savannah = {
 
     const view = `
     <div class="savannah  allGames">
-        <section class="savannah__startScreen  allGames__startScreen">
+        <section class="allGames__startScreen">
             <h1 class="allGames__heading">Саванна</h1>
             <p class="allGames__description">Тренировка Саванна развивает словарный запас. Чем больше слов ты
                 знаешь, тем больше очков опыта получишь.</p>
             <button class="allGames__startBtn  btn">Начать</button>
         </section>
 
-        <section class="savannah__timerScreen  allGames__timerScreen  allGames__timerScreen-hidden">
+        <section class="allGames__timerScreen  allGames__timerScreen-hidden">
             <div class="allGames__timer">3</div>
             <div class="allGames__tip">Используй клавиши 1, 2, 3 и 4, чтобы дать быстрый ответ</div>
         </section>
 
-        <section class="savannah__playScreen  allGames__playScreen  allGames__playScreen-hidden">
-            <div>Let's play</div>
+        <section class="savannah--playScreen  allGames__playScreen  allGames__playScreen-hidden">
+            <div class="savannah--controls">
+                <div class="savannah--sound"></div>
+                <div class="savannah--stars">
+                    <div class="savannah--stars__item"></div>
+                    <div class="savannah--stars__item"></div>
+                    <div class="savannah--stars__item"></div>
+                    <div class="savannah--stars__item"></div>
+                    <div class="savannah--stars__item"></div>
+                </div>
+            </div>
+            <div class="savannah--game">
+                <div class="savannah--game__question">word</div>
+                <div class="savannah--game__answers-list">
+                    <div class="savannah--game__answer"><span>1</span>неправильно</div>
+                    <div class="savannah--game__answer"><span>2</span>правильно</div>
+                    <div class="savannah--game__answer"><span>3</span>неправильно</div>
+                    <div class="savannah--game__answer"><span>4</span>неправильно</div>
+                </div>
+            </div>
         </section>
     </div>
     `;
