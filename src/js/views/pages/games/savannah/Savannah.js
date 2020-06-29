6a5094c64e036542e6dd9b6ea29f0085d67103ed
Kate <chelsea-92@mail.ru>
2020-06-29 21:30:23 +0300

    fix: selection of level

--- src/js/views/pages/games/savannah/Savannah.js
+++ src/js/views/pages/games/savannah/Savannah.js
@@ -310,23 +310,23 @@ const Savannah = {
                         <option value="1">1</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
-                        <option value="3">4</option>
-                        <option value="3">5</option>
-                        <option value="3">6</option>
-                        <option value="3">7</option>
-                        <option value="3">8</option>
-                        <option value="3">9</option>
-                        <option value="3">10</option>
-                        <option value="3">11</option>
-                        <option value="3">12</option>
-                        <option value="3">13</option>
-                        <option value="3">14</option>
-                        <option value="3">15</option>
-                        <option value="3">16</option>
-                        <option value="3">17</option>
-                        <option value="3">18</option>
-                        <option value="3">19</option>
-                        <option value="3">20</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                        <option value="11">11</option>
+                        <option value="12">12</option>
+                        <option value="13">13</option>
+                        <option value="14">14</option>
+                        <option value="15">15</option>
+                        <option value="16">16</option>
+                        <option value="17">17</option>
+                        <option value="18">18</option>
+                        <option value="19">19</option>
+                        <option value="20">20</option>
                     </select>
                 </div>
             </div>
@@ -414,6 +414,7 @@ const Savannah = {
         const data = await Savannah.model.getSetOfWordsAndTranslations(Savannah.difficulty,
           Savannah.round - 1, Savannah.gameRoundsCount, 3);
         Savannah.data = Savannah.reformat(data);
+        console.log(Savannah.data);
       } else {
         // заменить на уже изученные слова
         console.log('изученные слова');
