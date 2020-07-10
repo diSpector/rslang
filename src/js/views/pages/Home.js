35a16171186cd3277102391f5f4aa585250a9f1c
Elena Mironova <mironovahl@yandex.ru>
2020-07-10 13:01:55 +0400

    feat: add link and edit modal

--- src/js/views/pages/Home.js
+++ src/js/views/pages/Home.js
@@ -9,9 +9,9 @@ const Home = {
                 <div class="mainPage__content_promo">
                   <div class="mainPage_promo_image"></div>
                   <div class="mainPage_promo_text">
-                    <h2 class="mainPage_promo_title">Учить английский язык с RS Lang легко и интересно</h2>
+                    <h2 class="mainPage_promo_title">Английский язык - это просто</h2>
                     <p class="mainPage_promo_description">
-                      RS Lang - это приложение для нескучного и эффективного изучения английского языка, в котором ты будешь изучать новые слова, проверять свои знания в различных играх и зарабатывать в них очки, а также отслеживать свой прогресс.
+                      RS Lang - это приложение для веселого и эффективного изучения английского языка. Узнавай новые слова, закрепляй их с помощью мини-игр, зарабатывай очки и отслеживай свой прогресс, когда тебе удобно!
                     </p>
                     <div class="mainPage_promo_button">
                       <button class="mainPage_button-login" onclick="location.href='/#/login'">Войти</button>
@@ -21,36 +21,45 @@ const Home = {
                   <div class="mainPage_promo_description"></div>
                 </div>
                 <div class="mainPage__content_message hidden">
-                <div class="mainPage_message-card">
-                  <div class="mainPage_message-card_title">Раздел заблокирован</div>
-                  <p class="mainPage_message-card_description">Для того чтобы перейти в этот раздел, необходимо авторизоваться.</p>
-                  <button class="mainPage_message-card_button">Ok</button>
+                  <div class="mainPage_message-card">
+                    <div class="mainPage_message-card_title">
+                      <p>Раздел заблокирован</p>
+                      <button class="mainPage_message-buttonClose"></button>
+                    </div>
+                    <p class="mainPage_message-card_description">Для того чтобы перейти в этот раздел, необходимо авторизоваться.</p>
+                    <button class="mainPage_message-card_button" onclick="location.href='/#/login'">Войти</button>
+                  </div>
                 </div>
-              </div>
                 <div class="mainPage__content_blocks">
                   <div class="mainPage_block-card mainPage-bigCard"">
                     <h2 class="mainPage_block-title">Карточки</h2>
+                    <a class="mainPage-card-link" href="#/cards"></a>
                     <div class="mainPage-card-shadow"></div>
                     <div class="mainPage-card-locking"></div>
                   </div>
                   <div class="mainPage_block-game mainPage-smallCard">
                     <h2 class="mainPage_block-title">Игры</h2>
+                    <a class="mainPage-card-link" href="#/games/all"></a>
                     <div class="mainPage-card-shadow"></div>
                     <div class="mainPage-card-locking"></div>
                   </div>
                   <div class="mainPage_block-statistics mainPage-smallCard">
                     <h2 class="mainPage_block-title">Статистика</h2>
+                    <a class="mainPage-card-link" href="#/statistic"></a>
                     <div class="mainPage-card-shadow"></div>
                     <div class="mainPage-card-locking"></div>
                   </div>
-                  <div class="mainPage_block-team mainPage-smallCard" onclick="location.href='/#/team'">
+                  <div class="mainPage_block-team mainPage-smallCard">
                     <h2 class="mainPage_block-title">О команде</h2>
+                    <a class="mainPage-card-linkAll" href="#/team"></a>
                   </div>
-                  <div class="mainPage_block-promo mainPage-smallCard" onclick="location.href='/#/promo'">
+                  <div class="mainPage_block-promo mainPage-smallCard">
                     <h2 class="mainPage_block-title">Промо</h2>
+                    <a class="mainPage-card-linkAll" href="#/promo"></a>
                   </div>
                   <div class="mainPage_block-dictionary mainPage-bigCard">
                     <h2 class="mainPage_block-title">Словарь</h2>
+                    <a class="mainPage-card-link" href="#/dictionary"></a>
                     <div class="mainPage-card-shadow"></div>
                     <div class="mainPage-card-locking"></div>
                   </div>
@@ -71,27 +80,15 @@ const Home = {
         item.classList.add('hidden');
       });
       document.querySelector('.mainPage__content_promo').classList.add('hidden');
-      document.querySelector('.mainPage_block-card').onclick = () => {
-        window.location.replace(`${window.location.origin}/#/cards`);
-      };
-
-      document.querySelector('.mainPage_block-game').onclick = () => {
-        window.location.replace(`${window.location.origin}/#/games/all`);
-      };
-
-      document.querySelector('.mainPage_block-statistics').onclick = () => {
-        window.location.replace(`${window.location.origin}/#/statistic`);
-      };
-
-      document.querySelector('.mainPage_block-dictionary').onclick = () => {
-        window.location.replace(`${window.location.origin}/#/dictionary/:id`);
-      };
-
+    } else {
+      document.querySelectorAll('mainPage-card-link').forEach((item) => {
+        item.removeAttribute('href');
+      });
     }
     document.querySelector('.mainPage__content_blocks').addEventListener('click', ({ target }) => {
       if (target.classList.contains('mainPage-card-locking')) {
         document.querySelector('.mainPage__content_message').classList.remove('hidden');
-        document.querySelector('.mainPage_message-card_button').onclick = () => {
+        document.querySelector('.mainPage_message-buttonClose').onclick = () => {
           document.querySelector('.mainPage__content_message').classList.add('hidden');
         };
       }
