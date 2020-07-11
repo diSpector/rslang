9c9f6b0ea6b99c23c6937e292f5dd33779e13501
Viktor Makarov <gollwyd@mail.ru>
2020-07-11 18:53:23 +0300

    feat: new header

--- src/js/views/components/Header.js
+++ src/js/views/components/Header.js
@@ -4,7 +4,7 @@ import AppModel from '../../model/AppModel';
 
 const Header = {
   render: async () => {
-    const view = /* html */`
+    const view = /* `
     <div class="header__content wrapper">
   <div class="burger">
     <a id="hamburger-icon" class="open-close-btn">
@@ -27,28 +27,84 @@ const Header = {
       <li class="header__navItem"><a href="#/login" class ='logout'>Войти</a></li>
     </div>
   </div>
-</div>
-    `;
+</div>`
+html */
+`
+<div class="header__wrapper">
+            <div id="burger">
+                <button class="header__burger"></button>
+                <div class="header__burger-navigation notdisplay">
+                    <div class="header__burger-nav">
+                        <button class="header__burger rotate"></button>
+                        <a href="#" class="logo logo-burger">
+                            <h1>RS Lang</h1>
+                        </a>
+                        <nav class="header__navigation">
+                            <a href="#/cards">Изучение слов</a>
+                            <a href="#/stats">Статистика</a>
+                            <a href="#/dictionary">Словарь</a>
+                            <a href="#/games/all">Игры</a>
+                            <a href="#/promo">О приложении</a>
+                            <a href="#/team">Команда</a>
+                            <a href="#/login" class='logout'>Войти</a>
+                        </nav>
+                    </div>
+                </div>
+
+            </div>
+            <a href="#" class="logo">
+                <h1>RS Lang</h1>
+            </a>
+            <nav class="header__navigation">
+                <ul id="navigation">
+                    <li class="nav-item__navigation"><a href="#/cards">Изучение слов</a></li>
+                    <li class="nav-item__navigation"><a href="#/stats">Статистика</a></li>
+                    <li class="nav-item__navigation"><a href="#/dictionary">Словарь</a></li>
+                    <li class="nav-item__navigation"><a href="#/games/all">Игры</a></li>
+                    <li class="nav-item__navigation"><a href="#/login" class='logout'>Войти</a></li>
+                </ul>
+
+            </nav>
+        </div>
+`
+
+    ;
     return view;
   },
   afterRender: async () => {
     const model = new AppModel();
-
-    document.querySelector('.open-close-btn').addEventListener('click', (e) => {
+    const BURGER_BUTTON = document.querySelector('#burger');
+   /* document.querySelector('.open-close-btn').addEventListener('click', (e) => {
       e.preventDefault();
       document.querySelector('.overlay').classList.toggle('overlay-open');
       document.querySelector('#hamburger-icon').classList.toggle('hamburger-open');
     });
-    const log = document.querySelector('.logout');
+
+    */
+    BURGER_BUTTON.addEventListener('click', (event) => {
+    if (event.target.classList.contains('header__burger')) {
+        if (document.querySelector('.header__burger-navigation').classList.contains('notdisplay')) {
+            document.querySelector('.header__burger-navigation').classList.remove('notdisplay')
+        }
+        else {
+            document.querySelector('.header__burger-navigation').classList.add('notdisplay')
+        }
+    }
+})
+
+    const logList = document.querySelectorAll('.logout');
+    const logArray = Array.from(logList);
 
     if (await model.checkUser()){
-      log.onclick = () => model.logOutUser();
-      log.textContent = 'Выйти';
-      log.setAttribute('href', '#/');
+      logArray.forEach(log => {
+        log.onclick = () => model.logOutUser();
+        log.textContent = 'Выйти';
+        log.setAttribute('href', '#/');
+      });
+      
     }
 
     console.log(await model.checkUser());
-    
   },
 
 
